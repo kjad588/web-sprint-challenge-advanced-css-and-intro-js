85445f891a77dd2391323b47b7909db54391118d
Khalil Jad <j.khalil588@gmail.com>
2021-03-19 14:01:26 -0600

    completed task 6

--- index.js
+++ index.js
@@ -290,7 +290,16 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
+function addArtist(array){
+  array.push({ 
+    id: 20,
+    name: 'Khalil Jad', 
+    years: '1988 - 2021',
+    genre: 'Web Design', 
+    nationality: 'Palestinian',
+    bio: 'I am from colorado born and raised'
+  } );
+  return array;
     /*Your Code Here*/
   }
 
